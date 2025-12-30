3f5f84286da6cbaeacd0be69a8323422a38292c7
Shaunak Dugar <shaunakdugar@MacBook-Air-4.local>
2025-12-30 17:26:31 +0530

    fix small thing

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,6 @@ export default function Home() {
       gestureOrientation: "vertical",
       smoothWheel: true,
       wheelMultiplier: 1,
-      smoothTouch: false,
       touchMultiplier: 2,
       infinite: false,
     });
